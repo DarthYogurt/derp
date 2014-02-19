223f1a0fbda5c9cbe24d6884937776cbf6a18024
Raymond Walintukan <raytochina@gmail.com>
2014-02-18 17:54:42 -0800

    new models

--- derp_app/models.py
+++ derp_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=15)
+    
+    fbId = models.IntegerField()
+    fbName = models.CharField()
+    
+    def __unicode__(self):
+        return self.name + "-"+str(self.id)

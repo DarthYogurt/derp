8a2d1fe969587b43a1db1fa34cb70a1affd93d5b
Raymond Walintukan <raytochina@gmail.com>
2014-02-19 23:59:37 -0800

    modifying view

--- derp_app/admin.py
+++ derp_app/admin.py
@@ -6,3 +6,4 @@ from derp_app.models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Friend)
+admin.site.register(Picture)
--- derp_app/models.py
+++ derp_app/models.py
@@ -18,20 +18,16 @@ class User(models.Model):
 class Friend(models.Model):
     parentFriend = models.ForeignKey("User",related_name='parentFriend')
     friendId = models.ForeignKey("User",related_name='friendId')
-# #     fbId = models.ForeignKey("FacebookDb")
-# #     name = models.CharField(max_length=50, null=True, blank=True)
-# #     fbId = models.IntegerField()
-#     dateAdded = models.DateField(null=True, blank=True)
-#     
+   
     def __unicode__(self):
         return str(self.parentFriend.fbName) + " - " + str(self.friendId.fbName)
         
 
-# class Pic(models.Model):
-#     poster = models.ForeignKey("User")
-#     popularity = models.IntegerField(null=True, blank=True)
-#     upVote = models.IntegerField(null=True, blank=True)
-#     downvote = models.IntegerField(null=True, blank=True)
-#     views = models.IntegerField(null=True,blank=True)
-#     #image = models.ImageField(null=True, blank=True)
-#     targetFbId = models.IntegerField()
+class Picture(models.Model):
+    poster = models.ForeignKey("User", related_name="poster")
+    targetFbId = models.ForeignKey("User", related_name="targetUser")
+    popularity = models.IntegerField(null=True, blank=True)
+    upVote = models.IntegerField(null=True, blank=True)
+    downvote = models.IntegerField(null=True, blank=True)
+    views = models.IntegerField(null=True,blank=True)
+    image = models.FileField(upload_to="/derp_media/%Y/%m/%d")

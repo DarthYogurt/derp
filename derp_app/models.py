08bab998b96e2f36f7686015b42d1b7202929c62
Raymond Walintukan <raytochina@gmail.com>
2014-02-19 18:43:42 -0800

    working new data structure

--- derp_app/models.py
+++ derp_app/models.py
@@ -6,34 +6,32 @@ class User(models.Model):
     phone = models.CharField(max_length=20, null=True, blank=True)
     email = models.CharField(max_length=15, null=True, blank=True)
     
-    fbId = models.ForeignKey("FacebookDb", null=True,blank=True)
+    fbId = models.IntegerField(unique=True)
     fbName = models.CharField(max_length=40)
+    activated = models.BooleanField(default=False)
     
     def __unicode__(self):
-        return str(self.name) + "-" +str(self.id)
+        return str(self.fbName) + "-" +str(self.id) + " Active:" + str(self.activated)
 
-class FacebookDb(models.Model):
-    name = models.CharField(max_length=50, null=True, blank=True)
-    fbId = models.IntegerField()
-    dateAdded = models.DateField(null=True, blank=True) 
-       
+
+#        
 class Friend(models.Model):
-    parentFriend = models.ForeignKey("User",related_name='Friend_parentId')
-    friendId = models.ForeignKey("User",related_name='Friend_friendId')
-    fbId = models.ForeignKey("FacebookDb")
-#     name = models.CharField(max_length=50, null=True, blank=True)
-#     fbId = models.IntegerField()
-    dateAdded = models.DateField(null=True, blank=True)
-    
+    parentFriend = models.ForeignKey("User",related_name='parentFriend')
+    friendId = models.ForeignKey("User",related_name='friendId')
+# #     fbId = models.ForeignKey("FacebookDb")
+# #     name = models.CharField(max_length=50, null=True, blank=True)
+# #     fbId = models.IntegerField()
+#     dateAdded = models.DateField(null=True, blank=True)
+#     
     def __unicode__(self):
-        return str(self.id) + "-" + str(self.name)
-    
+        return str(self.parentFriend.fbName) + " - " + str(self.friendId.fbName)
+        
 
-class Pic(models.Model):
-    poster = models.ForeignKey("User")
-    popularity = models.IntegerField(null=True, blank=True)
-    upVote = models.IntegerField(null=True, blank=True)
-    downvote = models.IntegerField(null=True, blank=True)
-    views = models.IntegerField(null=True,blank=True)
-    #image = models.ImageField(null=True, blank=True)
-    targetFbId = models.IntegerField()
+# class Pic(models.Model):
+#     poster = models.ForeignKey("User")
+#     popularity = models.IntegerField(null=True, blank=True)
+#     upVote = models.IntegerField(null=True, blank=True)
+#     downvote = models.IntegerField(null=True, blank=True)
+#     views = models.IntegerField(null=True,blank=True)
+#     #image = models.ImageField(null=True, blank=True)
+#     targetFbId = models.IntegerField()

d31ffa1a9d4a4296e26ede074ccb9b8a836e18d9
Deric Walintukan <deric.walintukan@gmail.com>
2014-02-20 21:41:02 -0800

    added background, functions of buttons

--- android/src/com/walintukai/derpteam/MainActivity.java
+++ android/src/com/walintukai/derpteam/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -15,6 +17,22 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getActionBar().setTitle("");
+		
+		ImageView btnVoteDown = (ImageView) findViewById(R.id.btn_vote_down);
+		btnVoteDown.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(MainActivity.this, "Voted Down", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView btnVoteUp = (ImageView) findViewById(R.id.btn_vote_up);
+		btnVoteUp.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(MainActivity.this, "Voted Up", Toast.LENGTH_SHORT).show();
+			}
+		});
 		
 		ImageButton btnTakePicture = (ImageButton) findViewById(R.id.take_picture);
 		btnTakePicture.setOnClickListener(new OnClickListener() {

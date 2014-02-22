8e1d1559dfeab3f2290d0cfe479710842a2ad1a9
Raymond Walintukan <raytochina@gmail.com>
2014-02-21 16:05:52 -0800

    fixed try catch stopping on everyone

--- derp_app/views.py
+++ derp_app/views.py
@@ -13,60 +13,50 @@ from derp_app.models import *
 # Create your views here.
 @csrf_exempt
 def login(request):
-    try:
-        dataString = request.FILES.get('data', "empty")
-        if dataString == "empty":
-            return HttpResponse("Post Data Empty")
-        data = json.load(dataString)
-        
-        #for d in data:  print d,data[d]
-        
-        if User.objects.filter(fbId=data.get('fbUserId',0)).exists():
-            #print "User exists"
-            True
-            # need to update user info
-        else:
-            #print "not exist"
-            newUser = User(
-                           fbId = data.get('fbUserId',0),
-                           fbName = data.get("fbUserName", ""),
-                           activated = True,                   
-                           )
-            newUser.save()
     
-        
+    dataString = request.FILES.get('data', "empty")
+    if dataString == "empty":
+        return HttpResponse("Post Data Empty")
+    data = json.load(dataString)
+    
+    #for d in data:  print d,data[d]
+    
+    if User.objects.filter(fbId=data.get('fbUserId',0)).exists():
+        #print "User exists"
+        True
+        # need to update user info
+    else:
+        #print "not exist"
+        newUser = User(
+                       fbId = data.get('fbUserId',0),
+                       fbName = data.get("fbUserName", ""),
+                       activated = True,                   
+                       )
+        newUser.save()
+    
+    
         for friend in data['fbFriends']:
-            #fbName =""
-            print friend
-            fbName = friend['fbName'] #.encode("utf-8") #.encode("utf-8")   
-     
-            if User.objects.filter(fbId=friend.get('fbId',0)).exists():
-                #print "already in User Database"
-                #Update this friend again
-                updateUser = User.objects.get(fbId=friend.get('fbId',0))
-                updateUser.fbName = fbName #friend.get("fbName",0)
-                updateUser.save()
-            else:
-                newUser = User(
-                               fbId=friend.get("fbId",0),
-                               fbName= fbName, #friend.get("fbName",0),
-                               activated = False
-                               )
-                newUser.save()
-    #              
-    #         if Friend.objects.filter(parentFriend = User.objects.get(fbId =data.get('fbUserId',0)), 
-    #                                  friendId = User.objects.get(fbId = friend.get("fbId",0))
-    #                                  ).exists():
-    #             True #already friends can just do an update
-    #         else:
-    #             newFriend = Friend(
-    #                                parentFriend = User.objects.get(fbId=data.get("fbUserId",0)),
-    #                                friendId = User.objects.get(fbId=friend.get("fbId",0))
-    #                                )
-    #             newFriend.save()
-    
-    except:
-        print "skipped"
+            try:
+                #fbName =""
+                print friend
+                fbName = friend['fbName'] #.encode("utf-8") #.encode("utf-8")   
+         
+                if User.objects.filter(fbId=friend.get('fbId',0)).exists():
+                    #print "already in User Database"
+                    #Update this friend again
+                    updateUser = User.objects.get(fbId=friend.get('fbId',0))
+                    updateUser.fbName = fbName #friend.get("fbName",0)
+                    updateUser.save()
+                else:
+                    newUser = User(
+                                   fbId=friend.get("fbId",0),
+                                   fbName= fbName, #friend.get("fbName",0),
+                                   activated = False
+                                   )
+                    newUser.save()
+
+            except:
+                print "skipped"
         
     return HttpResponse("")
 

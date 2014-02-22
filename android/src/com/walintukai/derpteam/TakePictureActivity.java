e9045dbce62ed8f9de9f713e0a208af75e149cb8
Deric Walintukan <deric.walintukan@gmail.com>
2014-02-22 13:55:21 -0800

    shared image from other app now saves a copy to internal

--- android/src/com/walintukai/derpteam/TakePictureActivity.java
+++ android/src/com/walintukai/derpteam/TakePictureActivity.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,6 +16,7 @@ import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -33,9 +35,10 @@ public class TakePictureActivity extends Activity {
 	private static final int REQUEST_PICTURE = 1;
 	
 	private Preferences prefs;
+	private ImageView takenPicture;
 	private String filename;
 	private String oldFilename;
-	private ImageView takenPicture;
+	private File file;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +59,72 @@ public class TakePictureActivity extends Activity {
 			}
 		});
 		
-		if (filename.isEmpty()) {
-			new NewPictureThread().start();
+		// Handles image being sent from other application
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+		
+		if (Intent.ACTION_SEND.equals(action) && type != null) {
+			if (type.startsWith("image/")) { handleSentImage(intent); }
 		}
+		
+		if (filename.isEmpty()) { new NewPictureThread().start(); }
+	}
+	
+	private void handleSentImage(Intent intent) {
+		Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+		if (imageUri != null) {
+			String realPath = getRealPathFromUri(imageUri);
+			File extFile = new File(realPath);
+			filename = getImageFilename();
+			file = new File(getExternalFilesDir(null), filename);
+			copyFile(extFile, file);
+			Log.i("PICTURE COPIED TO INTERNAL", filename);
+		
+			compressAndRotateImage(file);
+			showPicture(file);
+		}
+		else { Log.e("RECEIVED IMAGE", "NULL"); }
+	}
+	
+	public String getRealPathFromUri (Uri contentUri) {
+		Cursor cursor = null;
+		try { 
+			String[] proj = { MediaStore.Images.Media.DATA };
+			cursor = getContentResolver().query(contentUri,  proj, null, null, null);
+			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			cursor.moveToFirst();
+			return cursor.getString(column_index);
+		} 
+		finally {
+			if (cursor != null) { cursor.close(); }
+		}
+	}
+	
+	public static void copyFile(File src, File dst) {
+	    FileChannel inChannel = null;
+		try { inChannel = new FileInputStream(src).getChannel(); } 
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+		
+	    FileChannel outChannel = null;
+		try { outChannel = new FileOutputStream(dst).getChannel(); } 
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+	    
+    	try { inChannel.transferTo(0, inChannel.size(), outChannel); } 
+    	catch (IOException e) { e.printStackTrace(); }
+    	finally {
+    		Log.i("FILE COPIED", "");
+    		
+    		if (inChannel != null) {
+    			try { inChannel.close(); } 
+    			catch (IOException e) { e.printStackTrace(); }
+    		}
+				
+    		if (outChannel != null) {
+	        	try { outChannel.close(); } 
+	        	catch (IOException e) { e.printStackTrace(); }
+	        }	
+	    }
 	}
 	
 	@Override
@@ -68,13 +134,13 @@ public class TakePictureActivity extends Activity {
 		if (!filename.isEmpty()) { GlobalMethods.deleteFileFromExternal(this, filename); }
 	}
 	
-	private void showPicture() {
+	private void showPicture(File file) {
 		try {
-    		File file = new File(getExternalFilesDir(null), filename);
 			FileInputStream fis = new FileInputStream(file);
-			Bitmap imgFromFile = BitmapFactory.decodeStream(fis);
+			Bitmap imgFile = BitmapFactory.decodeStream(fis);
 			fis.close();
-			takenPicture.setImageBitmap(imgFromFile);
+			
+			takenPicture.setImageBitmap(imgFile);
 			takenPicture.invalidate();
 		} 
     	catch (FileNotFoundException e) { e.printStackTrace(); } 
@@ -82,12 +148,12 @@ public class TakePictureActivity extends Activity {
 	}
 	
 	private String getImageFilename() {
-		filename = "fid_" + prefs.getFbUserId() + "_photo_" + getTimeStampForFilename() + ".jpg";
-		return filename;
+		return getTimeStampForFilename() + "-" + prefs.getFbUserId() + ".jpg";
+		
 	}
 	
 	private static String getTimeStampForFilename() {
-		SimpleDateFormat sdf = new SimpleDateFormat("MMddyy_HHmmss");
+		SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
 		String now = sdf.format(new Date());
 		return now;
 	}
@@ -112,8 +178,7 @@ public class TakePictureActivity extends Activity {
 	
 	private void startCameraActivity() {
 		filename = getImageFilename();
-		
-		File file = new File(getExternalFilesDir(null), filename);
+		file = new File(getExternalFilesDir(null), filename);
 		Uri outputFileUri = Uri.fromFile(file);
 		
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -132,15 +197,15 @@ public class TakePictureActivity extends Activity {
 			if (!oldFilename.isEmpty()) { GlobalMethods.deleteFileFromExternal(this, oldFilename); }
 			
 			Log.i("PICTURE SAVED", filename);
-			compressAndRotateImage(this, filename);
-			showPicture();
+			
+			file = new File(getExternalFilesDir(null), filename);
+			compressAndRotateImage(file);
+			showPicture(file);
 		}
 	}
 		
-	private static void compressAndRotateImage(Context context, String filename) {
+	private void compressAndRotateImage(File file) {
 		try {
-			File file = new File(context.getExternalFilesDir(null), filename);
-			
 		    // Decode image size
 		    BitmapFactory.Options o = new BitmapFactory.Options();
 		    o.inJustDecodeBounds = true;
@@ -162,7 +227,7 @@ public class TakePictureActivity extends Activity {
 		    
 		    // Get orientation of picture
 		    ExifInterface exif = null;
-			try { exif = new ExifInterface(context.getExternalFilesDir(null) + "/" + filename); } 
+			try { exif = new ExifInterface(getExternalFilesDir(null) + "/" + filename); } 
 			catch (IOException e1) { e1.printStackTrace(); }
 			int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
 			Log.i("ORIENTATION", Integer.toString(orientation));
@@ -207,11 +272,12 @@ public class TakePictureActivity extends Activity {
 	private Intent createShareIntent() {
 		Intent intent = new Intent(Intent.ACTION_SEND);
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
 		intent.setType("image/*");
-		
 		File file = new File(getExternalFilesDir(null), filename);
 		Uri uri = Uri.fromFile(file);
 		intent.putExtra(Intent.EXTRA_STREAM, uri);
+		
 		return intent;
 	}
 	

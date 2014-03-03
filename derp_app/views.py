955b3a763b83cbfba474d94a5fd3a115585be59d
Raymond Walintukan <raytochina@gmail.com>
2014-03-02 17:11:11 -0800

    houseclean

--- derp_app/views.py
+++ derp_app/views.py
@@ -247,13 +247,7 @@ def vote(request):
     
     pic.save()
     
-    
-    if not Vote.objects.filter(user = User.objects.get(fbId= data.get("fbUserId",1)), picture = Picture.objects.get(id= data.get("picId"), 1)).exists():
-        newVote = Vote(user = User.objects.get(fbId= data.get("fbUserId",1)),
-                       picture = Picture.objects.get(id= data.get("picId"), 1),
-                       voteUp = data.get("voteUp", "null")
-                       )
-        newVote.save()
+
 
     return HttpResponse("Vote Added")
     

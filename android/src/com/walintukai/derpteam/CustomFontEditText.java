3791811e45de3ab744b1f5a7287a292133f6cc6d
Deric Walintukan <deric.walintukan@gmail.com>
2014-03-13 01:09:47 -0700

    changed font of edit texts

---
+++ android/src/com/walintukai/derpteam/CustomFontEditText.java
@@ -0,0 +1,32 @@
+package com.walintukai.derpteam;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class CustomFontEditText extends EditText {
+
+	public CustomFontEditText(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	public CustomFontEditText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public CustomFontEditText(Context context) {
+		super(context);
+		init();
+	}
+
+	private void init() {
+		if (!isInEditMode()) {
+			Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/SourceSansPro-Regular.otf");
+			setTypeface(tf);
+		}
+	}
+
+ }
